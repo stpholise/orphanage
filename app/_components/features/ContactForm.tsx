572873b3ce796381eff1d5f68f48b87cb9db9842
stpholise <stpholise@gmail.com>
2025-04-30 21:17:09 +0100

    add descktop contact page

---
+++ app/_components/features/ContactForm.tsx
@@ -0,0 +1,157 @@
+"use client";
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
+import * as Yup from "yup";
+import clsx from "clsx";
+
+interface ValueTypes {
+  firstName: string;
+  lastName: string;
+  email: string;
+  subject: string;
+  message: string;
+}
+
+const ContactForm = () => {
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    subject: "",
+    message: "",
+  };
+
+  const validation = Yup.object({
+    firstName: Yup.string(),
+    lastName: Yup.string(),
+    email: Yup.string().email(),
+    subject: Yup.string(),
+    message: Yup.string(),
+  });
+
+  const contactFormSubmission = (
+    values: ValueTypes,
+    { resetForm }: FormikHelpers<ValueTypes>
+  ) => {
+    console.log("Form submitted:", values);
+    resetForm();
+  };
+
+  return (
+    <div className="container mx-auto lg:w-[760px] py-8 my-8">
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validation}
+        onSubmit={contactFormSubmission}
+      >
+        {({ isSubmitting, dirty, isValid }) => (
+          <Form className="flex flex-col gap-8">
+            <div className=" grid grid-cols-2 gap-8">
+              <div className=" w-full ">
+                <label
+                  htmlFor="firstName"
+                  className="block text-xs font-bold text-[#1D2130]"
+                >
+                  First Name
+                </label>
+                <Field
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  className="outline-none px-2 py-1 border-b-2 broder-gray-300 w-full"
+                />
+                <ErrorMessage
+                  name="firstName"
+                  component={"div"}
+                  className="text-sm text-red-600"
+                />
+              </div>
+              <div className="w-full ">
+                <label
+                  htmlFor="lastName"
+                  className="block text-xs font-bold text-[#1D2130]"
+                >
+                  Last Name
+                </label>
+                <Field
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  className="outline-none px-2 py-1 border-b-2 broder-gray-300 w-full"
+                />
+                <ErrorMessage
+                  name="lastName"
+                  component={"div"}
+                  className="text-sm text-red-600"
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="email"
+                  className="block text-xs font-bold text-[#1D2130]"
+                >
+                  Email
+                </label>
+                <Field
+                  type="text"
+                  name="email"
+                  id="email"
+                  className="outline-none px-2 py-1 border-b-2 broder-gray-300 w-full"
+                />
+                <ErrorMessage
+                  name="email"
+                  component={"div"}
+                  className="text-sm text-red-600"
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  htmlFor="subject"
+                  className="block text-xs font-bold  text-[#1D2130]"
+                >
+                  Subject
+                </label>
+                <Field
+                  type="text"
+                  name="subject"
+                  id="subject"
+                  className="outline-none px-2 py-1 border-b-2 broder-gray-300 w-full"
+                />
+                <ErrorMessage
+                  name="subject"
+                  component={"div"}
+                  className="text-sm text-red-600"
+                />
+              </div>
+            </div>
+            <div className="w-full">
+              <label htmlFor="message">message</label>
+              <Field
+                as="textarea"
+                name="message"
+                id="message"
+                className="outline-none border-2 broder-gray-200 w-full h-36 rounded-sm"
+              />
+            </div>
+            <div className="mx-auto">
+              <button
+                disabled={!isValid || isSubmitting || !dirty}
+                className={clsx(" w-44 h-10 ", {
+                  "bg-gray-300 text-gray-500 cursor-not-allowed":
+                    !isValid || isSubmitting || !dirty,
+                  "bg-[#F2C94C] hover:bg-[#e6be3a] active:bg-[#d9b334] text-[#1D2130]":
+                    isValid && !isSubmitting && dirty,
+                  "bg-[#F2C94C]/80 cursor-wait":
+                    isSubmitting && isValid && dirty,
+                })}
+              >
+                send message
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default ContactForm;
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,51 @@
+import SectionTitle from "../_components/ui/SectionTitle";
+import ContactForm from "../_components/features/ContactForm";
 const page = () => {
-  return <div className="">contact</div>;
+  return (
+    <div className="">
+      <div className="lg:h-[580px] w-full bg-[#FCEDC6] lg:py-16 ">
+        <div className="container mx-auto px-4 md:px-6 flex flex-row lg:gap-28  lg:px-8">
+          <div className="">
+            <SectionTitle title={"Contact us"} />
+            <div className=" lg:ml-16 lg:w-[480px] flex-col gap-4">
+              <h2 className="text-[52px] font-bold">
+                We&apos;d love to hear from you
+              </h2>
+              <p className="">
+                Have any question in mind or want to enquire? Please feel free
+                to contact us through the form or the following details.
+              </p>
+            </div>
+          </div>
+          <div className="lg:w-80 h-96 flex flex-col items-stretch lg:gap-8">
+            <div className="border-b border-gray-200 h-1/3">
+              <h3 className="md:text-xl font-bold mb-4">Let&apos;s talk! </h3>
+              <div className=" flex gap-4 pb-3">
+                <p className="">+234 09012346514</p>
+                <p>hello@largerthani.com</p> 
+              </div>
+            </div>
+            <div className="h-1/3">
+              <h3 className="md:text-xl font-bold mb-4">Headoffice</h3>
+              <div>
+                <p className="">8 Brewery Drive, Lagos,</p>
+                <p>Nigeria.</p> 
+              </div>
+            </div>
+            <div className="  h-1/3">
+              <h3 className="md:text-xl font-bold mb-4">Branch Office</h3>
+              <div>
+                <p className="">Opp Opolo round about, Yenagoa, Bayelsa,</p>
+                <p>Nigeria.</p> 
+              </div>
+                      </div>
+                      <div className="">foot</div>
+          </div>
+        </div>
+      </div>
+      <ContactForm />
+    </div>
+  );
 };
 
 export default page;
