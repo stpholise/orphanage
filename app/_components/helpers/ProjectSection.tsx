41b41a73ec53b2013145bbe40c8fb33a532ca12b
stpholise <stpholise@gmail.com>
2025-05-07 04:47:53 +0100

    service page mobile

--- app/_components/helpers/ProjectSection.tsx
+++ app/_components/helpers/ProjectSection.tsx
@@ -4,9 +4,9 @@ import SectionTitle from "../ui/SectionTitle";
 const ProjectSection = () => {
   return (
     <div className="">
-      <div className="container mx-auto flex flex-col gap-9 md:gap-12 lg:px-8 px-4 py-9  md:py-12 lg:py-14">
+      <div className="container mx-auto flex flex-col gap-6 sm:gap-9 md:gap-12 lg:px-8 px-4 py-9  md:py-12 lg:py-14">
         <div className="flex gap-9 md:gap-12 lg:gap-16 items-center">
-          <div className="flex flex-col gap-9  ">
+          <div className="flex flex-col sm:gap-6 gap-3 md:gap-9  ">
             <SectionTitle title={"projects we have done"} />
             <div className=" ml-2 lg:w-[607px] flex flex-col gap-8 xl:ml-16 lg:ml-16 ">
               <h2 className=" text-3xl sm:text-4xl md:text-5xl font-bold">
